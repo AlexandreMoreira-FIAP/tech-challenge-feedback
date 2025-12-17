f33373d6ee6abfd9fd58465bbdacc01e02a74f55
Alexandre Moreira <alexandre.dellaestudos@gmail.com>
2025-12-16 21:45:42 -0300

    feat: subindo a fila e os logs da aplicação

--- infra/main.tf
+++ infra/main.tf
@@ -12,30 +12,54 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.app_name}"
   location = var.location
 }
 
-# Banco de Dados Postgres
-resource "azurerm_postgresql_flexible_server" "db_server" {
-  name                = "psql-${var.app_name}"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_log_analytics_workspace" "analytics" {
+  name                = "log-${var.app_name}"
   location            = azurerm_resource_group.rg.location
-  version             = "13"
-
-  administrator_login    = "psqladmin"
-  administrator_password = var.db_password
-
-  storage_mb   = 32768
-  storage_tier = "P4"
-  sku_name     = "B_Standard_B1ms"
-
-  zone = "1"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
-# Database
+resource "azurerm_application_insights" "app_insights" {
+  name                = "insights-${var.app_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.analytics.id
+  application_type    = "java"
+}
+
+resource "azurerm_storage_account" "sa_app" {
+  name                     = "stapp${replace(var.app_name, "-", "")}" # Remove hifens
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#Fila (Queue)
+resource "azurerm_storage_queue" "queue" {
+  name                 = "feedback-urgente-queue"
+  storage_account_name = azurerm_storage_account.sa_app.name
+}
+
+resource "azurerm_postgresql_flexible_server" "db_server" {
+  name                   = "psql-${var.app_name}"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  version                = "13"
+  administrator_login    = "psqladmin"
+  administrator_password = var.db_password
+  storage_mb             = 32768
+  storage_tier           = "P4"
+  sku_name               = "B_Standard_B1ms"
+  zone                   = "1"
+}
+
 resource "azurerm_postgresql_flexible_server_database" "db" {
   name      = "feedbackdb"
   server_id = azurerm_postgresql_flexible_server.db_server.id
@@ -43,7 +67,6 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
   charset   = "utf8"
 }
 
-# Regra de Firewall (Libera acesso interno Azure)
 resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure" {
   name             = "AllowAzureServices"
   server_id        = azurerm_postgresql_flexible_server.db_server.id
@@ -51,16 +74,41 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure" {
   end_ip_address   = "0.0.0.0"
 }
 
-# App Service Plan (Linux B1)
 resource "azurerm_service_plan" "app_plan" {
   name                = "plan-${var.app_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "B1" # B1 suporta Java Web App
 }
 
-# Web App
+resource "azurerm_linux_function_app" "fn_app" {
+  name                = "func-${var.app_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.app_plan.id
+
+  storage_account_name       = azurerm_storage_account.sa_app.name
+  storage_account_access_key = azurerm_storage_account.sa_app.primary_access_key
+
+  site_config {
+    application_stack {
+      java_version = "17"
+    }
+  }
+
+  app_settings = {
+    "DB_URL"      = "jdbc:postgresql://${azurerm_postgresql_flexible_server.db_server.fqdn}:5432/feedbackdb"
+    "DB_USER"     = "psqladmin"
+    "DB_PASSWORD" = var.db_password
+
+    # Monitoramento
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.app_insights.connection_string
+
+    "FUNCTIONS_WORKER_RUNTIME" = "java"
+  }
+}
+
 resource "azurerm_linux_web_app" "app" {
   name                = "app-${var.app_name}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -73,19 +121,22 @@ resource "azurerm_linux_web_app" "app" {
       java_server_version = "17"
       java_version        = "17"
     }
-
     app_command_line = "java -jar /home/site/wwwroot/app.jar"
   }
 
-  # Configurações de ambiente
   app_settings = {
-    "DB_URL"            = "jdbc:postgresql://${azurerm_postgresql_flexible_server.db_server.fqdn}:5432/feedbackdb"
-    "DB_USER"           = "psqladmin"
-    "DB_PASSWORD"       = var.db_password
-    "WEBSITES_PORT"     = "80"
-    "QUARKUS_HTTP_PORT" = "80"
+    "DB_URL"      = "jdbc:postgresql://${azurerm_postgresql_flexible_server.db_server.fqdn}:5432/feedbackdb"
+    "DB_USER"     = "psqladmin"
+    "DB_PASSWORD" = var.db_password
+
+    "WEBSITES_PORT"                     = "80"
+    "QUARKUS_HTTP_PORT"                 = "80"
     "QUARKUS_SWAGGER_UI_ALWAYS_INCLUDE" = "true"
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
 
+    "AZURE_STORAGE_CONNECTION_STRING" = azurerm_storage_account.sa_app.primary_connection_string
+    "QUEUE_NAME"                      = azurerm_storage_queue.queue.name
+
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.app_insights.connection_string
   }
 }
